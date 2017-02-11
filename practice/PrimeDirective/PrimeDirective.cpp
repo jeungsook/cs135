11a609fff8957d41d1db16fc10a4c292955ef526
Jeung-Sook Williams <willij6@unlv.nevada.edu>
2017-02-11 05:41:15 -0800

    Add files via upload

---
+++ practice/PrimeDirective/PrimeDirective.cpp
@@ -0,0 +1,55 @@
+//##############################################################//
+//				Prime Directive									//					
+//		Enter a number, and this program will tell				//
+//				you if it's prime or not						//
+//##############################################################//
+
+#include <iostream> // cout, cin
+
+int main() {
+  
+	// Variables
+	int num;
+	int den = 2;
+	//
+
+	// Read the number
+	std::cout << "Input a number: ";
+	std::cin >> num;
+	//
+ 
+	// If the number is less than 2, it can't be prime
+	if (num < 2)
+		std::cout << num << " is not a prime number." << std::endl;
+
+	// If the number is 2, it is prime
+    else if (num == 2)
+		std::cout << "2 is a prime number." << std::endl;
+
+	// Divide the increasing variable den until den = num
+    else {   
+
+		while (den < num) {
+	
+			// A number other than num and 1 divides evenly into num, it is not prime
+			if (num % den == 0) {
+				std::cout << num << " is not a prime number." << std::endl;
+				den = num;
+			}
+
+			else {
+
+				den++;
+				
+				// No number other than num and 1 divides evenly into num, it is prime
+				if (den == num)
+      				std::cout << num << " is a prime number." << std::endl;
+
+			}
+
+        }
+    }
+
+ return 0;
+
+}
